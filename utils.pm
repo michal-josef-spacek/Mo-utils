44b1d88af904c8a9ef0982c61be9d12fe53d8afe
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-12-11 14:11:21 +0100

    Prepare for 0.04 version

--- utils.pm
+++ utils.pm
@@ -10,7 +10,7 @@ use Readonly;
 Readonly::Array our @EXPORT_OK => qw(check_array_object check_isa
 	check_number check_number_of_items check_required);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 sub check_array_object {
 	my ($self, $key, $class, $class_name) = @_;
@@ -376,6 +376,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
