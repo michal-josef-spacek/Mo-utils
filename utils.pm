658cdb13f05a28ff608a846411f0a8d11008ebd4
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-10-19 12:46:02 +0200

    Add example for check_number_of_items()

--- utils.pm
+++ utils.pm
@@ -353,6 +353,92 @@ Returns undef.
  use strict;
  use warnings;
 
+ use Test::MockObject;
+
+ $Error::Pure::TYPE = 'Error';
+
+ use Mo::utils qw(check_number_of_items);
+
+ # Item object #1.
+ my $item1 = Test::MockObject->new;
+ $item1->mock('value', sub {
+ 	return 'value1',
+ });
+
+ # Item object #1.
+ my $item2 = Test::MockObject->new;
+ $item2->mock('value', sub {
+ 	return 'value2',
+ });
+
+ # Tested object.
+ my $self = Test::MockObject->new({
+ 	'key' => [],
+ });
+ $self->mock('list', sub {
+ 	return [
+ 		$item1,
+ 		$item2,
+ 	];
+ });
+
+ # Check number of items.
+ check_number_of_items($self, 'list', 'value', 'Test', 'Item');
+
+ # Print out.
+ print "ok\n";
+
+ # Output like:
+ # ok
+
+=head1 EXAMPLE9
+
+ use strict;
+ use warnings;
+
+ use Test::MockObject;
+
+ $Error::Pure::TYPE = 'Error';
+
+ use Mo::utils qw(check_number_of_items);
+
+ # Item object #1.
+ my $item1 = Test::MockObject->new;
+ $item1->mock('value', sub {
+ 	return 'value1',
+ });
+
+ # Item object #2.
+ my $item2 = Test::MockObject->new;
+ $item2->mock('value', sub {
+ 	return 'value1',
+ });
+
+ # Tested object.
+ my $self = Test::MockObject->new({
+ 	'key' => [],
+ });
+ $self->mock('list', sub {
+ 	return [
+ 		$item1,
+ 		$item2,
+ 	];
+ });
+
+ # Check number of items.
+ check_number_of_items($self, 'list', 'value', 'Test', 'Item');
+
+ # Print out.
+ print "ok\n";
+
+ # Output like:
+ # #Error [...utils.pm:?] Test for Item 'value1' has multiple values.
+
+=head1 EXAMPLE10
+
+ use strict;
+ use warnings;
+
  use Mo::utils qw(check_required);
 
  my $self = {
@@ -366,7 +452,7 @@ Returns undef.
  # Output:
  # ok
 
-=head1 EXAMPLE9
+=head1 EXAMPLE11
 
  use strict;
  use warnings;
