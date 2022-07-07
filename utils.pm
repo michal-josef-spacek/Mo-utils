c052c15d87c788d89c2cc52af7de809fcbb05a62
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-07-07 12:01:56 +0200

    Prepare for 0.10 version

--- utils.pm
+++ utils.pm
@@ -11,7 +11,7 @@ use Scalar::Util qw(blessed);
 Readonly::Array our @EXPORT_OK => qw(check_array check_array_object check_bool
 	check_isa check_length check_number check_number_of_items check_required);
 
-our $VERSION = 0.09;
+our $VERSION = 0.10;
 
 sub check_array {
 	my ($self, $key) = @_;
@@ -665,6 +665,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.09
+0.10
 
 =cut
