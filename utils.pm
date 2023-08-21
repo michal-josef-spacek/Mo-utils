82134f40b2a27c5b6022fe6a1fd4c66de3e200a0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-08-21 19:31:01 +0200

    Prepare for 0.20 version

--- utils.pm
+++ utils.pm
@@ -13,7 +13,7 @@ Readonly::Array our @EXPORT_OK => qw(check_array check_array_object check_array_
 	check_bool check_code check_isa check_length check_number check_number_of_items
 	check_regexp check_required check_string_begin check_strings);
 
-our $VERSION = 0.19;
+our $VERSION = 0.20;
 
 sub check_array {
 	my ($self, $key) = @_;
@@ -1143,6 +1143,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.19
+0.20
 
 =cut
