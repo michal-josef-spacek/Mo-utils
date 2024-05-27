381432f099230ee912ac66b9cc3455c82cd6bbb7
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-05-27 09:53:37 +0200

    Prepare for 0.30 version

--- utils.pm
+++ utils.pm
@@ -15,7 +15,7 @@ Readonly::Array our @EXPORT_OK => qw(check_angle check_array check_array_object
 	check_number_of_items check_number_range check_regexp check_required
 	check_string check_string_begin check_strings);
 
-our $VERSION = 0.29;
+our $VERSION = 0.30;
 
 sub check_angle {
 	my ($self, $key) = @_;
@@ -1643,6 +1643,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.29
+0.30
 
 =cut
